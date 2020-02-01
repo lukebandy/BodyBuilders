98582d4512587cdd260a9606c717021432db273e
lukebandy <60064928+lukebandy@users.noreply.github.com>
2020-02-01 22:02:37 +0000

    Squelching Noises

---
+++ Assets/Scripts/BodyPart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour {
+    public List<AudioClip> audioClips;
+    AudioSource audioSource;
+    float audioTimeout;
+
+    // Start is called before the first frame update
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        audioTimeout -= Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (audioTimeout <= 0) {
+            audioSource.clip = audioClips[Random.Range(0, 3)];
+            audioSource.Play();
+            audioTimeout = 2.0f;
+        }
+    }
+}
